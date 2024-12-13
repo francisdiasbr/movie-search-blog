3f49065368727bb29f9e086686048ef699af4a4f
francisdiasbr <francisdiasbr@gmail.com>
2024-12-13 18:24:00 -0300

    Layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -17,15 +17,19 @@ export function Layout({ children }: LayoutProps) {
   return (
     <div className="flex flex-col min-h-screen bg-white dark:bg-slate-950">
       <Topbar />
-      <SubTopbar />
-      {isInternalPage && (
-        <div className={`relative ${isMobile ? 'ml-4 mt-4' : 'ml-4 mt-4'} z-10`}>
-          <GoBack />
+      <div className="flex justify-center flex-grow">
+        <div className="w-full max-w-[1200px] flex flex-col">
+          {isInternalPage && (
+            <div className={`relative ${isMobile ? 'ml-4 mt-4' : 'ml-4 mt-4'} z-10`}>
+              <GoBack />
+            </div>
+          )}
+          <SubTopbar />
+          <main className="flex-grow pt-6 mt-4">
+            {children}
+          </main>
         </div>
-      )}
-      <main className="flex-grow pt-6 mt-4">
-        {children}
-      </main>
+      </div>
       <Footer />
     </div>
   );
